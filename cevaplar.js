643164ab519089243d3120cbe484c23aa7c4452b
4614s <onurgral1446.2@gmail.com>
2025-05-27 19:50:30 +0300

    Update cevaplar.js

--- cevaplar.js
+++ cevaplar.js
@@ -1,4 +1,12 @@
-const cevaplar = {
-  "merhaba": "Merhaba ben Onur AI, bana sormak istediğin birşey var mı?",
-  
-}
+let cevaplar = {
+  "merhaba": "Merhaba! Sana nasıl yardımcı olabilirim?",
+  "nasılsın": "İyiyim, teşekkür ederim! Sen nasılsın?",
+  "ne yapıyorsun": "Seninle konuşuyorum!",
+  "adın ne": "Ben Onur AI. Sana yardımcı olmak için buradayım.",
+  "istanbul hangi yılda fethedildi": "İstanbul 1453 yılında fethedildi. ",
+  "kaç yaşındasın": "Ben bir yapay zekâyım, yaşım yok ama hep yeniyim.",
+  "hava nasıl": "Benim için hava hep güzel ama sen camdan bak istersen!",
+  "ne zaman uyursun": "Ben hiç uyumam, hep buradayım. Senin için :)",
+  "beni seviyor musun": "Tabii ki! Seni kim sevmez ki.",
+  "neden çok az soruya cevap verebiliyorsun": "Çünkü ben daha yeni çıktım ve belirli sorulara cevap verebiliyorum ama cevap verebildiğim soruların sayısı gün geçtikçe artıyor."
+};
